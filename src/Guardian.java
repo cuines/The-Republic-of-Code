19f55141be043a1bbb5e905732fc2b12cebb0ed3
cuines <fionaleslief7698@outlook.com>
2025-12-31 00:17:06 +0800

    Refactor Guardian class: rename variables, add effectiveness score

--- src/Guardian.java
+++ src/Guardian.java
@@ -1,42 +1,42 @@
 public class Guardian {
-    private String name;
-    private int strength;
-    private int wisdom;
-    private boolean isOnDuty;
+    private String guardianName;
+    private int str;
+    private int wis;
+    private boolean dutyFlag;
     
-    public Guardian(String name, int strength, int wisdom) {
-        this.name = name;
-        this.strength = strength;
-        this.wisdom = wisdom;
-        this.isOnDuty = false;
+    public Guardian(String guardianName, int str, int wis) {
+        this.guardianName = guardianName;
+        this.str = str;
+        this.wis = wis;
+        this.dutyFlag = false;
     }
     
     public void performDuty() {
-        if (isOnDuty) {
-            if (strength > 50) {
-                if (wisdom > 30) {
-                    System.out.println("Guardian " + name + " is performing duty with excellence.");
+        if (dutyFlag) {
+            if (str > 50) {
+                if (wis > 30) {
+                    System.out.println("Guardian " + guardianName + " is performing duty with excellence.");
                 } else {
-                    System.out.println("Guardian " + name + " lacks wisdom, duty may be flawed.");
+                    System.out.println("Guardian " + guardianName + " lacks wisdom, duty may be flawed.");
                 }
             } else {
-                System.out.println("Guardian " + name + " is too weak to perform duty.");
+                System.out.println("Guardian " + guardianName + " is too weak to perform duty.");
             }
         } else {
-            System.out.println("Guardian " + name + " is not on duty.");
+            System.out.println("Guardian " + guardianName + " is not on duty.");
         }
     }
     
     public void setOnDuty(boolean onDuty) {
-        isOnDuty = onDuty;
+        dutyFlag = onDuty;
     }
     
     public boolean evaluateGuardian() {
-        if (strength > 70 && wisdom > 60) {
+        if (str > 70 && wis > 60) {
             return true;
-        } else if (strength > 50 && wisdom > 40) {
+        } else if (str > 50 && wis > 40) {
             return true;
-        } else if (strength > 30 && wisdom > 20) {
+        } else if (str > 30 && wis > 20) {
             return true;
         } else {
             return false;
@@ -44,10 +44,19 @@ public class Guardian {
     }
     
     public String getStatus() {
-        if (isOnDuty) {
+        if (dutyFlag) {
             return "On duty";
         } else {
             return "Off duty";
         }
+    }
+    
+    // New method to compute guardian's effectiveness score
+    public int computeEffectiveness() {
+        int score = 0;
+        if (str > 50) score += 10;
+        if (wis > 40) score += 10;
+        if (dutyFlag) score += 5;
+        return score;
     }
 }
